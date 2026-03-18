93ed5ae84dd41c2d403dda07daea5a95a5f81c32
lakhdar hafsi <lacdart2@hotmail.com>
2026-03-18 06:00:57 +0100

    feat: capacitor setup for native Android and iOS

---
+++ android/app/src/main/java/no/alesundmasjid/app/MainActivity.java
@@ -0,0 +1,5 @@
+package no.alesundmasjid.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'no.alesundmasjid.app',
+  appName: 'Alesund Masjid',
+  webDir: 'out',
+  ios: {
+    contentInset: 'always'
+  }
+};
+
+export default config;
